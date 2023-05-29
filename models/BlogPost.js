f4a3289544f4c77052651163d982fa65760d754b
lan-yin <jay.braden1128@gmail.com>
2023-05-29 14:44:13 +0800

    initiate commit

---
+++ models/BlogPost.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const blogPostSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+    contentOne: {
+      type: String,
+      require: true,
+    },
+    contentTwo: {
+      type: String,
+    },
+    author: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+export default BlogPost;
